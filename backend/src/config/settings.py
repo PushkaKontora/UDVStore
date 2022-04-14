1892c0842f151acf571828dae18263c868512bc5
Yura Perov <iury.perov@gmail.com>
2022-04-14 23:56:57 +0500

    Changed dbms to sqlite3

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -93,15 +93,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql_psycopg2",
+#             "NAME": env("POSTGRES_DB"),
+#             "USER": env("POSTGRES_USER"),
+#             "PASSWORD": env("POSTGRES_PASSWORD"),
+#             "HOST": env("POSTGRES_HOST"),
+#             "PORT": env("POSTGRES_PORT"),
+#         }
+# }
+
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": env("POSTGRES_DB"),
-            "USER": env("POSTGRES_USER"),
-            "PASSWORD": env("POSTGRES_PASSWORD"),
-            "HOST": env("POSTGRES_HOST"),
-            "PORT": env("POSTGRES_PORT"),
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "config" / "db.sqlite3",
+    }
 }
 
 # Password validation
