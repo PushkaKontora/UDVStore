f802b606c19a792d6003f0ffea2743abaf0f7076
SeriousDim <dlykov2001@yandex.ru>
2022-04-17 19:03:14 +0500

    Cors must working maybe

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -174,8 +174,8 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:4200",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:4200",
 ]
 
 # DJOSER = {
