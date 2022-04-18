9f7538642be57238bdf99d291ecf476c8bd69436
Yura Perov <iury.perov@gmail.com>
2022-04-18 12:07:41 +0500

    Init users and products in db for frontend. Added MethodField 'name' into .product/StorageCellSerializer

--- backend/src/api/internal/product/serializers/StorageCellSerializer.py
+++ backend/src/api/internal/product/serializers/StorageCellSerializer.py
@@ -1,9 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
+from api.internal.models.store.storage_cells.SizeChoices import SizeChoices
 from api.models import StorageCell
 
 
 class StorageCellSerializer(ModelSerializer):
+    name = SerializerMethodField(method_name="get_name")
+
+    def get_name(self, cell: StorageCell) -> str:
+        return SizeChoices(cell.size).name
+
     class Meta:
         model = StorageCell
         exclude = ("product",)
