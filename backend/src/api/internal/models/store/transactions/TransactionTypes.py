fa5d736758ee00f659f8ed126c760ae0fe7592e8
Yura Perov <iury.perov@gmail.com>
2022-05-02 13:01:20 +0500

    Added canceled and approved types into TransactionTypes

--- backend/src/api/internal/models/store/transactions/TransactionTypes.py
+++ backend/src/api/internal/models/store/transactions/TransactionTypes.py
@@ -5,4 +5,6 @@ class TransactionTypes(IntegerChoices):
     BUYING = 1  # покупка
     DEPOSIT = 2  # начисление коинов от админа, подтвержение активности
     COIN_GIFTING = 3  # дарение коинов
-    ACCEPT = 4  # прошение об активности
+    REQUEST = 4  # прошение об активности
+    CANCELED = 5
+    APPROVED = 6
