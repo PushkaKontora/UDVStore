ff5876422b4d370a8ce84ded43e4fb4c763abf0e
Yura Perov <iury.perov@gmail.com>
2022-05-11 20:29:25 +0500

    Frontend is in docker

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -6,6 +6,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.authtoken")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
