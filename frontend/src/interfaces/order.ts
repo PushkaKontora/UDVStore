5bcd8cbfee1ecec6a95d5c724c69983c6d3c9309
Yura Perov <iury.perov@gmail.com>
2022-05-21 13:33:40 +0500

    Fixed getting orders

--- frontend/src/interfaces/order.ts
+++ frontend/src/interfaces/order.ts
@@ -1,24 +1,19 @@
-import {cell, products} from "./interfaces";
 import {IProfile} from "./profile";
 
 export interface IOrder {
-    "id": number,
-    "created_at": string,
-    "order": {
-        "id": number,
-        "profile": IProfile,
-        "status": number,
-        "product": {
-            "id": number,
-            "name": string,
-            "photo": string,
-            "description": string,
-            "price": number,
-            "size": number,
-            "amount": number
-        },
-        "amount": number,
-        "in_cart": boolean,
-        "total": number
-    }
+    id: number,
+    profile: IProfile,
+    status: number,
+    product: {
+        id: number,
+        name: string,
+        photo: string,
+        description: string,
+        price: number,
+        size: number,
+        amount: number,
+    },
+    amount: number,
+    in_cart: boolean,
+    total: number,
 }
