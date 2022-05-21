22abad8be2278e10953ff76124ee1f3c26bede9b
SeriousDim <dlykov2001@yandex.ru>
2022-05-22 00:18:30 +0500

    /admin/requests modal accept & discard windows done

--- frontend/src/app/admin-main-page/components/admin-requests/admin-request.component.ts
+++ frontend/src/app/admin-main-page/components/admin-requests/admin-request.component.ts
@@ -10,6 +10,8 @@ import {map} from "rxjs";
 import {ITransactionFile} from "../../../../interfaces/transaction_file";
 import {FilemanagerService} from "../../services/filemanager.service";
 import {ModalWorker} from "../../services/modalworker.service";
+import {RequestComponent} from "./request-component/request.component";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'personal-area',
@@ -19,9 +21,16 @@ import {ModalWorker} from "../../services/modalworker.service";
 export class AdminRequestComponent implements OnInit {
 
     MAX_COMMENT_LENGTH = 2048
+    DISCARDED_MESSAGE = "Запрос отклонен".toUpperCase()
+    APPROVED_MESSAGE = "UCoins НАЧИСЛЕНЫ!"
 
     requests: ITransaction[]
+
+    public currentRequestComponent: RequestComponent | null
     acceptForm: FormGroup
+    discardForm: FormGroup
+
+    message: String | null
 
     /*
     public accrualInputControl = new FormControl(0, [
@@ -32,25 +41,39 @@ export class AdminRequestComponent implements OnInit {
     public commentInputControl = new FormControl('')*/
 
     constructor(private _service: RequestService,
-                public _modals: ModalWorker) {
+                public _modals: ModalWorker,
+                private _router: Router) {
 
     }
 
     ngOnInit(): void {
         this.getRequestData()
         this.createAcceptForm()
+        this.createDiscardForm()
+
+        console.log("message = " + this.message)
     }
 
     createAcceptForm() {
-        var coins = new FormControl(1, [
+        const coins = new FormControl(1, [
             Validators.required, Validators.min(1)
-        ])
-        var comment = new FormControl("", [
+        ]);
+        const comment = new FormControl("", [
             Validators.maxLength(this.MAX_COMMENT_LENGTH)
-        ])
+        ]);
 
         this.acceptForm = new FormGroup({
             coins: coins,
+            comment: comment
+        })
+    }
+
+    createDiscardForm() {
+        const comment = new FormControl("", [
+            Validators.maxLength(this.MAX_COMMENT_LENGTH)
+        ]);
+
+        this.discardForm = new FormGroup({
             comment: comment
         })
     }
@@ -64,8 +87,52 @@ export class AdminRequestComponent implements OnInit {
             })
     }
 
-    public tryAccept() {
-        this._service.approve()
+    public closeAcceptModal() {
+        this.currentRequestComponent = null
+        this._modals.closeModal('modal-accept')
+    }
+
+    public closeDiscardModal() {
+        this.currentRequestComponent = null
+        this._modals.closeModal('modal-discard')
+    }
+
+    public submitAccept() {
+        if (this.currentRequestComponent) {
+            var amount = this.acceptForm.controls["coins"].value
+            var comment = this.acceptForm.controls["comment"].value
+
+            this._service.approve(this.currentRequestComponent.data, amount, comment)
+                .subscribe(() => {
+                    console.log("Coins are accrued")
+                    this.message = this.APPROVED_MESSAGE
+                    //window.location.reload()
+                    var idx = this.requests.findIndex(t => {
+                        return JSON.stringify(t) === JSON.stringify(this.currentRequestComponent?.data)
+                    })
+                    this.requests.splice(idx, 1)
+                    this.closeAcceptModal()
+                })
+        }
+    }
+
+    public submitDiscard() {
+        if (this.currentRequestComponent) {
+            var comment = this.discardForm.controls["comment"].value
+
+            this._service.cancel(this.currentRequestComponent.data,
+                                 comment)
+                .subscribe(() => {
+                    console.log("Coins are discarded")
+                    this.message = this.DISCARDED_MESSAGE
+                    //window.location.reload()
+                    var idx = this.requests.findIndex(t => {
+                        return JSON.stringify(t) === JSON.stringify(this.currentRequestComponent?.data)
+                    })
+                    this.requests.splice(idx, 1)
+                    this.closeDiscardModal()
+                })
+        }
     }
 
 }
--- frontend/src/app/admin-main-page/components/admin-requests/request-component/request.component.ts
+++ frontend/src/app/admin-main-page/components/admin-requests/request-component/request.component.ts
@@ -4,6 +4,7 @@ import {RequestService} from "../../../services/request.service";
 import {ITransaction} from "../../../../../interfaces/transaction";
 import {ModalWorker} from "../../../services/modalworker.service";
 import {FilemanagerService} from "../../../services/filemanager.service";
+import {AdminRequestComponent} from "../admin-request.component";
 
 @Component({
   selector: 'app-request-component',
@@ -15,7 +16,8 @@ export class RequestComponent implements OnInit {
     @Input() data: ITransaction
 
     constructor(private _service: RequestService,
-                public _modals: ModalWorker) {
+                public _modals: ModalWorker,
+                private _parent: AdminRequestComponent) {
 
     }
 
@@ -32,11 +34,15 @@ export class RequestComponent implements OnInit {
     }
 
     public approve() {
-        //this._service.approve()
+        this._parent.currentRequestComponent = this
+        this._modals.openModal('modal-accept')
+        console.log(this._parent.currentRequestComponent)
     }
 
     public discard() {
-
+        this._parent.currentRequestComponent = this
+        this._modals.openModal('modal-discard')
+        console.log(this._parent.currentRequestComponent)
     }
 
 }
