bd18b98f0ee6bf8c69a7c5f8cf2a44979d3a22c2
tania <taniakovesh@gmail.com>
2022-05-30 10:36:55 +0500

    доработки

--- frontend/src/app/personal-area/components/personal-area-wrapper/personal-area-wrapper.component.ts
+++ frontend/src/app/personal-area/components/personal-area-wrapper/personal-area-wrapper.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {IUser} from "../../../../interfaces/interfaces";
+import {PeopleService} from "../../../login/services/people.service";
 
 @Component({
-  selector: 'app-personal-area-wrapper',
-  templateUrl: './personal-area-wrapper.component.html',
-  styleUrls: ['./personal-area-wrapper.component.scss']
+    selector: 'app-personal-area-wrapper',
+    templateUrl: './personal-area-wrapper.component.html',
+    styleUrls: ['./personal-area-wrapper.component.scss']
 })
 export class PersonalAreaWrapperComponent implements OnInit {
+    public user?: IUser;
 
-  constructor(private _router: Router) {
-      _router.navigate(["/main-page/personal-area/orders"]);
-  }
+    constructor(private _peopleService: PeopleService, private _router: Router) {
+        this._peopleService.findUser.subscribe((res) => {
+            this.user = res
+        });
+        _router.navigate(["/main-page/personal-area/orders"]);
+    }
 
-  ngOnInit(): void {
-  }
+
+    ngOnInit(): void {
+    }
 
 }
