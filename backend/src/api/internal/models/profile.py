a467a54a4865d34c134ff3715aaec0c1cda08126
Iuri Perov <iury.perov@gmail.com>
2022-07-11 22:30:28 +0500

    Fixed type of balance

--- backend/src/api/internal/models/profile.py
+++ backend/src/api/internal/models/profile.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -6,7 +7,12 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     patronymic = models.CharField(max_length=256, default="", blank=True)
-    balance = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    balance = models.DecimalField(
+        max_digits=settings.COINS_AMOUNT_DIGITS,
+        decimal_places=settings.COINS_DECIMAL_PLACES,
+        validators=[MinValueValidator(0)],
+        default=0,
+    )
     photo = models.ImageField(upload_to="profile", null=True)
 
     class Meta:
